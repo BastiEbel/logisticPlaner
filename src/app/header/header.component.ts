c30279709f034a991c708ffde55ab64e6c45ff0c
Sebastian Ebel <sebastianebel@hotmail.de>
2022-08-20 22:08:07 +0200

    style and header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  collapsed = true;
-  constructor() {}
+  /* closeResult: string; */
+  constructor(private offcanvasService: NgbOffcanvas) {}
 
   ngOnInit(): void {}
+
+  openEnd(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { position: 'end' });
+  }
 }
